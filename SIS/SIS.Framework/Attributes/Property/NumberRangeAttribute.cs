b0b7f355d630902c84c528d3eab2c7e309c42680
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-10-18 20:46:04 +0300

    Added some attributes, being used to validate our binding models.

---
+++ SIS/SIS.Framework/Attributes/Property/NumberRangeAttribute.cs
@@ -0,0 +1,21 @@
+﻿namespace SIS.Framework.Attributes.Property
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class NumberRangeAttribute : ValidationAttribute
+    {
+        private readonly double minimumValue;
+        private readonly double maximumValue;
+
+        public NumberRangeAttribute(double minValue = double.MinValue, double maxValue = double.MaxValue)
+        {
+            this.minimumValue = minValue;
+            this.maximumValue = maxValue;
+        }
+
+        public override bool IsValid(object value)
+        {
+            return (double) value >= this.minimumValue && (double) value <= this.maximumValue;
+        }
+    }
+}
