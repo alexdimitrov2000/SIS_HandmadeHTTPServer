b6a817e199a28399c941f33eeb6a6aeab17bc6b4
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-10-19 00:20:37 +0300

    Commented the ConfigureRoutes method.

--- SIS/IRunesWebApp/Launcher.cs
+++ SIS/IRunesWebApp/Launcher.cs
@@ -26,39 +26,39 @@
             MvcEngine.Run(server);
         }
 
-        private static void ConfigureRoutes(ServerRoutingTable serverRoutingTable)
-        {
-            // GET REQUESTS
-            //serverRoutingTable.Routes[HttpRequestMethod.Get]["/"] 
-            //        = request => new HomeController().Index(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Home/Index"]
-                    = serverRoutingTable.Routes[HttpRequestMethod.Get]["/"];
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Users/Login"]
-                    = request => new UsersController().Login();
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Users/Register"]
-                    = request => new UsersController().Register();
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Users/Logout"]
-                    = request => new UsersController().Logout(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Albums/All"]
-                    = request => new AlbumsController().All();
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Albums/Create"]
-                    = request => new AlbumsController().Create();
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Albums/Details"]
-                    = request => new AlbumsController().Details(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Tracks/Create"]
-                    = request => new TracksController().Create(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Get]["/Tracks/Details"]
-                    = request => new TracksController().Details(request);
+        //private static void ConfigureRoutes(ServerRoutingTable serverRoutingTable)
+        //{
+        //    // GET REQUESTS
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/"]
+        //            = request => new HomeController().Index(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Home/Index"]
+        //            = serverRoutingTable.Routes[HttpRequestMethod.Get]["/"];
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Users/Login"]
+        //            = request => new UsersController().Login();
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Users/Register"]
+        //            = request => new UsersController().Register();
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Users/Logout"]
+        //            = request => new UsersController().Logout(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Albums/All"]
+        //            = request => new AlbumsController().All();
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Albums/Create"]
+        //            = request => new AlbumsController().Create();
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Albums/Details"]
+        //            = request => new AlbumsController().Details(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Tracks/Create"]
+        //            = request => new TracksController().Create(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Get]["/Tracks/Details"]
+        //            = request => new TracksController().Details(request);
 
-            // POST REQUESTS
-            serverRoutingTable.Routes[HttpRequestMethod.Post]["/Users/Login"]
-                    = request => new UsersController().DoLogin(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Post]["/Users/Register"]
-                    = request => new UsersController().DoRegister(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Post]["/Albums/Create"]
-                    = request => new AlbumsController().DoCreate(request);
-            serverRoutingTable.Routes[HttpRequestMethod.Post]["/Tracks/Create"]
-                    = request => new TracksController().DoCreate(request);
-        }
+        //    // POST REQUESTS
+        //    serverRoutingTable.Routes[HttpRequestMethod.Post]["/Users/Login"]
+        //            = request => new UsersController().DoLogin(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Post]["/Users/Register"]
+        //            = request => new UsersController().DoRegister(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Post]["/Albums/Create"]
+        //            = request => new AlbumsController().DoCreate(request);
+        //    serverRoutingTable.Routes[HttpRequestMethod.Post]["/Tracks/Create"]
+        //            = request => new TracksController().DoCreate(request);
+        //}
     }
 }
