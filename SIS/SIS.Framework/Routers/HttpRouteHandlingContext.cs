b4ee9601f26ba6ffc685067c702585cfd0021ca7
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-10-20 18:48:58 +0300

    Added an IHttpRouterContext interface, implemented by the new HttpRouteHandlingContext class.

---
+++ SIS/SIS.Framework/Routers/HttpRouteHandlingContext.cs
@@ -0,0 +1,44 @@
+﻿namespace SIS.Framework.Routers
+{
+    using HTTP.Common;
+    using HTTP.Requests.Contracts;
+    using HTTP.Responses.Contracts;
+    using WebServer.Api;
+
+    using System.Linq;
+
+    public class HttpRouteHandlingContext : IHttpRouterContext
+    {
+        private readonly IHttpHandler controllerRouter;
+
+        private readonly IHttpHandler resourceRouter;
+
+        public HttpRouteHandlingContext(IHttpHandler controllerRouter, IHttpHandler resourceRouter)
+        {
+            this.controllerRouter = controllerRouter;
+            this.resourceRouter = resourceRouter;
+        }
+
+        public IHttpResponse Handle(IHttpRequest request)
+        {
+            if (this.IsResourceRequest(request.Path))
+                return this.resourceRouter.Handle(request);
+
+            return this.controllerRouter.Handle(request);
+        }
+
+        private bool IsResourceRequest(string path)
+        {
+            if (path.Contains(GlobalConstants.Dot))
+            {
+                var lastIndexOfDot = path.LastIndexOf(GlobalConstants.Dot);
+
+                var resourceExtension = path.Substring(lastIndexOfDot);
+
+                return GlobalConstants.ResourceExtensions.Contains(resourceExtension);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ SIS/SIS.WebServer/Api/IHttpRouterContext.cs
@@ -0,0 +1,10 @@
+﻿namespace SIS.WebServer.Api
+{
+    using HTTP.Requests.Contracts;
+    using HTTP.Responses.Contracts;
+
+    public interface IHttpRouterContext
+    {
+        IHttpResponse Handle(IHttpRequest request);
+    }
+}
