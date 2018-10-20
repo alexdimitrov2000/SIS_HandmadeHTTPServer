4cbf81360fd443afb6f117d9b3ce8add07615c8b
alexdimitrov2000 <alex_dimitrov2000@abv.bg>
2018-10-20 18:49:57 +0300

    Added a ResourceRouter class, which handles resource file requests.

---
+++ SIS/SIS.Framework/Routers/ResourceRouter.cs
@@ -0,0 +1,34 @@
+﻿namespace SIS.Framework.Routers
+{
+    using SIS.HTTP.Common;
+    using SIS.HTTP.Enums;
+    using SIS.HTTP.Requests.Contracts;
+    using SIS.HTTP.Responses.Contracts;
+    using SIS.WebServer.Api;
+    using SIS.WebServer.Results;
+    using System.IO;
+
+    public class ResourceRouter : IHttpHandler
+    {
+        public IHttpResponse Handle(IHttpRequest request)
+        {
+            return this.ResourceFile(request.Path);
+        }
+
+
+        private IHttpResponse ResourceFile(string requestPath)
+        {
+            string resourcePath = requestPath.Substring(1);
+
+            if (!File.Exists(resourcePath))
+            {
+                string notFoundContent = File.ReadAllText(GlobalConstants.NotFoundFilePath);
+                return new BadRequestResult(notFoundContent);
+            }
+
+            var resourceContent = File.ReadAllBytes(resourcePath);
+
+            return new InlineResourceResult(resourceContent, HttpResponseStatusCode.Ok);
+        }
+    }
+}
